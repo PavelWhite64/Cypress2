ccb0f61f3352910d48c77010af4bcba31bbe2873
Pavel <white03041996@gmail.com>
2023-10-10 17:11:18 +0300

    Готово

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "rya5nt",
   e2e: {
     baseUrl: "http://qamid.tmweb.ru",
     setupNodeEvents(on, config) {
